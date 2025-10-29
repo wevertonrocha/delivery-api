74a314d10009708a6decb17e92519adaeda7095c
Weverton Rocha <wevertonrocha@yahoo.com>
2025-10-28 21:08:43 -0300

    primeiro commit

---
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/ProjetoDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.Delivery.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeliveryApiApplication.class, args);
+	}
+
+}
