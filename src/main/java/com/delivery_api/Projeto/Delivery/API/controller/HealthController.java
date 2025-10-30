7cc45a0552707a428a7bb9ad28258a36068d507d
Weverton Rocha <wevertonrocha@yahoo.com>
2025-10-29 22:09:05 -0300

    refatorando a classe healthcontroller

--- src/main/java/com/delivery_api/Projeto/Delivery/API/controller/HealthController.java
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/controller/HealthController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public Map<string, string> health(){
+    public Map<String, String> health(){
        return Map.of(
                "status", "UP",
                "timestamp", LocalDateTime.now().toString(),
